e03c9f9a5a7eec6024b9fa417ade31522176bb19
Till Kundrun <tillkundrun1@yahoo.de>
2019-10-02 10:36:01 +0200

    first commit

--- app.js
+++ app.js
@@ -3,17 +3,23 @@ require("dotenv").config();
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const express = require("express");
-const favicon = require("serve-favicon");
 const mongoose = require("mongoose");
 const logger = require("morgan");
 const path = require("path");
 
+// WHEN INTRODUCING USERS DO THIS:
+// INSTALL THESE DEPENDENCIES: passport-local, passport, bcrypt, express-session
+// AND UN-COMMENT OUT FOLLOWING LINES:
+
 const session = require("express-session");
-const MongoStore = require("connect-mongo")(session);
-const flash = require("connect-flash");
+const passport = require("passport");
+
+require("./configs/passport");
 
 mongoose
-  .connect("mongodb://localhost/last-man-standing", { useNewUrlParser: true })
+  .connect("mongodb://localhost/project-management-server", {
+    useNewUrlParser: true
+  })
   .then(x => {
     console.log(
       `Connected to Mongo! Database name: "${x.connections[0].name}"`
@@ -36,40 +42,32 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-// Express View engine setup
 
-app.use(express.static(path.join(__dirname, "public")));
-app.use(favicon(path.join(__dirname, "public", "images", "favicon.ico")));
+// ADD SESSION SETTINGS HERE:
 
-hbs.registerHelper("ifUndefined", (value, options) => {
-  if (arguments.length < 2)
-    throw new Error("Handlebars Helper ifUndefined needs 1 parameter");
-  if (typeof value !== undefined) {
-    return options.inverse(this);
-  } else {
-    return options.fn(this);
-  }
-});
+const MongoStore = require("connect-mongo")(session);
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: new MongoStore({ mongooseConnection: mongoose.connection })
+  })
+);
+// USE passport.initialize() and passport.session() HERE:
+app.use(passport.initialize());
+app.use(passport.session());
 
 // default value for title local
 app.locals.title = "Express - Generated with IronGenerator";
 
-// Enable authentication using session + passport
-app.use(
-  session({
-    secret: "irongenerator",
-    resave: true,
-    saveUninitialized: true,
-    store: new MongoStore({ mongooseConnection: mongoose.connection })
-  })
-);
-app.use(flash());
-require("./passport")(app);
+// ROUTES MIDDLEWARE STARTS HERE:
 
 const index = require("./routes/index");
 app.use("/", index);
 
+
 const authRoutes = require("./routes/auth");
-app.use("/auth", authRoutes);
+app.use("/api/auth", authRoutes);
 
 module.exports = app;
