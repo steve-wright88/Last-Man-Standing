51afb0291db4919317fa601a9abd47008e6d94b8
Steve <stewright88@gmail.com>
2019-10-02 17:13:15 +0200

    merge

--- routes/teamChoice.js
+++ routes/teamChoice.js
@@ -5,23 +5,6 @@ const Choice = require("../models/Choice");
 
 router.post("/", (req, res) => {
   // const { title, description, tasks = [] } = req.body;
-  const title = req.body.title;
-  const description = req.body.description;
-  const tasks = [];
-  const owner = req.user._id;
-
-  Project.create({
-    title: title,
-    description: description,
-    tasks: tasks,
-    owner: owner
-  })
-    .then(project => {
-      res.json(project);
-    })
-    .catch(err => {
-      res.json(err);
-    });
 });
 
 module.exports = router;
