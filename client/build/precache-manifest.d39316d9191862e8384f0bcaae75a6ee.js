7f765ff903fe7ec602038ac4238d8992e309a444
Steve <stewright88@gmail.com>
2019-10-08 21:08:46 +0200

    build folder

---
+++ client/build/precache-manifest.d39316d9191862e8384f0bcaae75a6ee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0245ca46158abcaef0bbb3a3cac8663",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5e69f82026c494a6cfd5",
+    "url": "/static/css/main.43acc8d8.chunk.css"
+  },
+  {
+    "revision": "6be088e8b87530bc0e36",
+    "url": "/static/js/2.812e283f.chunk.js"
+  },
+  {
+    "revision": "5e69f82026c494a6cfd5",
+    "url": "/static/js/main.e5bbaf33.chunk.js"
+  },
+  {
+    "revision": "7f45875b99a5ef02142c",
+    "url": "/static/js/runtime-main.c2763ce9.js"
+  }
+]);
